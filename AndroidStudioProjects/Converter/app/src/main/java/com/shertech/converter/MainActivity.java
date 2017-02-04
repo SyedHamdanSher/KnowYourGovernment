ae5046e7c05b22654b38c4de4c043af686da00aa
[lastwalker] <[syedhamdansher@gmail.com]>
2017-02-04 16:52:43 -0600

    Final app V1

--- AndroidStudioProjects/Converter/app/src/main/java/com/shertech/converter/MainActivity.java
+++ AndroidStudioProjects/Converter/app/src/main/java/com/shertech/converter/MainActivity.java
@@ -1,19 +1,22 @@
 package com.shertech.converter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btConvert;
+    Button btConvert,btCH;
     EditText etLT;
     TextView tvResult,aTV;
     RadioButton rb;
@@ -25,34 +28,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btConvert = (Button) findViewById(R.id.btConvert);
+        btCH = (Button) findViewById(R.id.btCH);
         etLT = (EditText) findViewById(R.id.etLT);
         tvResult = (TextView) findViewById(R.id.tvResult);
+        tvResult.setMovementMethod(new ScrollingMovementMethod());
         rg = (RadioGroup) findViewById(R.id.radioGroup);
         aTV = (TextView) findViewById(R.id.aTV);
-
+        aTV.setMovementMethod(new ScrollingMovementMethod());
     }
 
     private double tempCal(int rb)
     {
         double flag;
+        Context c;
+        CharSequence t;
         String F;
         if(rb == R.id.rbC2F)
         {
-            flag = ((Double.parseDouble(etLT.getText().toString())) * 9.0/5.0) + 32.0;
-            DecimalFormat df = new DecimalFormat("#.#");
-            F=df.format(flag);
-            aTV.append("C to F: "+ Double.parseDouble(etLT.getText().toString())+"->"+F+"\n");
-            aTV.setText(aTV.getText());
-            return flag;
+            if(etLT.getText().toString().matches("")){
+                c = getApplicationContext();
+                t = "Please enter some value to Convert";
+                Toast.makeText(c,t, Toast.LENGTH_SHORT).show();
+                return 999999;
+            }else {
+                flag = ((Double.parseDouble(etLT.getText().toString())) * 9.0 / 5.0) + 32.0;
+                F = String.format("%.1f", flag);
+                aTV.setText("C to F: " + Double.parseDouble(etLT.getText().toString()) + " -> " + F + "\n" + aTV.getText());
+                return flag;
+            }
         }else if (rb == R.id.rbF2C){
-            flag = ((Double.parseDouble(etLT.getText().toString())) - 32.0)* 5.0/9.0;
-            DecimalFormat df = new DecimalFormat("#.#");
-            F=df.format(flag);
-            aTV.append("F to C: "+ Double.parseDouble(etLT.getText().toString())+"->"+F+"\n");
-            aTV.setText(aTV.getText());
-            return flag;
+            if(etLT.getText().toString().matches("")){
+                c = getApplicationContext();
+                t = "Please Enter some value to Convert";
+                Toast.makeText(c,t, Toast.LENGTH_SHORT).show();
+                return 999999;
+            }else {
+                flag = ((Double.parseDouble(etLT.getText().toString())) - 32.0) * 5.0 / 9.0;
+                F = String.format("%.1f", flag);
+                aTV.setText("F to C: " + Double.parseDouble(etLT.getText().toString()) + " -> " + F + "\n" + aTV.getText());
+                return flag;
+            }
         }else{
-            return 0.0;
+            return 999999;
         }
     }
 
@@ -61,11 +78,30 @@ public class MainActivity extends AppCompatActivity {
         int rbID= rg.getCheckedRadioButtonId();
         rb = (RadioButton) findViewById(rbID);
         double flag;
+        String F;
 
         flag=tempCal(rbID);
-        //flag=Math.round(100 * flag) / 100d;
-        DecimalFormat df = new DecimalFormat("#.#");
-        String F=df.format(flag);
-        tvResult.setText(F);
+        if(flag != 999999){
+            F=String.format("%.1f",flag);
+            tvResult.setText(F);
+        }
+    }
+
+    public void onClear(View view){
+        aTV.setText("");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("aTV",aTV.getText().toString());
+        outState.putString("tvResult",tvResult.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        aTV.setText(savedInstanceState.getString("aTV"));
+        tvResult.setText(savedInstanceState.getString("tvResult"));
     }
 }
